50f7d93fa7712fef77b2e2d9831dd64c94ae7918
Kazuya N <macchi.machi@gmail.com>
2016-06-23 18:58:34 +0900

    second commit

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-# -*- cording: utf-8 -*-
-
 #音声認識結果テキストファイル読み込み
-f = open("text.txt","r")
-
+f = open("./julius4/text.txt","r")
 texts = []
 for line in f:
 	if line[:10] == "sentence1:":
@@ -15,22 +12,24 @@ print("\n---------音声認識---------\n", end ='')
 for t in texts:
   print(t)
 
+
 #形態素解析
+
 import MeCab
 m = MeCab.Tagger ("-Ochasen")
-a = []
 
 print("\n\n---------形態素解析---------\n", end ='')
 
-mtxt = []
-
+mtxt = []       #ttに入った解析結果を改行区切りでmtxt配列に入れる
 for tt in texts:
 	mtxt.append(m.parse(tt).split("\n"))
-print(mtxt)
-print("\n\n\n")
+#print(mtxt)
+
+print("\n\n\n") #mtxt配列から末尾のEOSを除いてmword配列に入れる
 for m in mtxt:
 	for mword in m[:-2]:
-		wordclass = mword.split("\t")
-		if "名詞" in wordclass[3]:
+		wordclass = mword.split("\t") #tab区切りでmwordをwordclass配列に入れる
+		if "名詞" in wordclass[3]:  #wordclassの3番目の要素に"名詞"が含まれる場合にその単語と品詞を表示
 			print(wordclass[0],wordclass[3])
+
 
